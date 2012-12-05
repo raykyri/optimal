21f635766ce11ce85f549536d1f12581ecea7220
Raymond Zhong <raykyri@gmail.com>
2012-12-05 06:21:02 -0500

    Add function presets and interpolation functions for them; refactor some code for clarity

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
+/* ****************************************************************************************************
+   Globals
+*/
+
 var board;
+var state = {
+  copy_mode: false,
+  functionmodel: JXG.Math.Numerics.lagrangePolynomial
+};
 
 var board_options = {
   showCopyright: false,
@@ -25,17 +33,105 @@ var function_options = {
   withLabel: false
 };
 
+// reset the app
+function reset() {
+  $.each(dataTables(), function(i,o) {
+    o.clear();
+  });
+  $('#datatables').empty();
+  initializeBoard();
+}
+
+// initialize the app
+function initialize() {
+  initializeBoard();
+  //$('#box').aToolTip();
+}
+
+/* ****************************************************************************************************
+   Presets tab
+*/
+
+// initialize the graph with preset points and a type of function
+function setModel(model) {
+  switchModel(model);
+  importPoints(presets[model]);
+}
+
+// switch the type of function graphed
+function switchModel(model) {
+  switch (model) {
+  case 'linear':
+    state.functionmodel = function(points) {
+      return JXG.Math.Numerics.regressionPolynomial(1, points);
+    };
+    break;
+  case 'quadratic':
+    state.functionmodel = function(points) {
+      return JXG.Math.Numerics.regressionPolynomial(2, points);
+    };
+    break;
+  case 'cubic':
+    state.functionmodel = function(points) {
+      return JXG.Math.Numerics.regressionPolynomial(3, points);
+    };
+    break;
+  case 'quartic':
+    state.functionmodel = function(points) {
+      return JXG.Math.Numerics.regressionPolynomial(4, points);
+    };
+    break;
+  case 'exponential':
+    state.functionmodel = state.functionmodel; //TODO
+    break;
+  case 'logistic':
+    state.functionmodel = state.functionmodel; //TODO
+    break;
+  case 'logarithmic':
+    state.functionmodel = state.functionmodel; //TODO
+    break;
+  case 'step':
+    state.functionmodel = state.functionmodel; //TODO
+    break;
+  case 'gaussian':
+    state.functionmodel = state.functionmodel; //TODO
+    break;
+  case 'interpolated':
+    state.functionmodel = JXG.Math.Numerics.CatmullRomSpline;
+    break;
+  case 'polynomial':
+    state.functionmodel = JXG.Math.Numerics.lagrangePolynomial;
+    break;
+  default:
+    alert('Invalid model to fit function to');
+  }
+}
+
+/* ****************************************************************************************************
+   Function Editor tab
+*/
+
 function dataTables() {
   return $.map($('.datatable'), function(o,i) {
     return $('.datatable:eq(' + i +')').data().handsontable;
   });
 }
 
+// set up the jQuery data table
+function initializeTable() {
+}
+
+/* ****************************************************************************************************
+   Import Data tab
+*/
+
+// import data from the textarea
 function importData() {
+
   // sanitize data
   var points = [];
-  var data = $('#datatable').val()
-    .split('\n');
+  var data = $('#datatable').val().trim().split('\n');
+  if (data.length === 0) return;
   $.each(data, function(i,o) {
     var point = o.split(/[,\ \t]+/);
     if (point.length === 0) return;
@@ -43,7 +139,11 @@ function importData() {
     point = $.map(point, function(o,i) { return o*1; }); // cast to float
     points.push(point);
   });
+  importPoints(points);
+}
 
+// import data from points onto the graph
+function importPoints(points) {
   // calculate bounds of data
   var min_x, min_y, max_x, max_y;
   $.each(points, function(i,o) {
@@ -56,7 +156,7 @@ function importData() {
   var range_x = max_x - min_x;
   var range_y = max_y - min_y;
   var margin = 0.1;
-  
+
   // reset graph viewport
   initializeBoard([
     min_x - range_x * margin,
@@ -90,30 +190,9 @@ function importData() {
   $('.nav-tabs a[href="#functions"]').tab('show');
 }
 
-function update() {
-  var points = $.map(data, function(o,i) {
-    return [[o[0]*1, o[1]*1]]; // create floats from decimal or scientific notation
-  });
-}
-
-// reset the app
-function reset() {
-  $.each(dataTables(), function(i,o) {
-      o.clear();
-  });
-  $('#datatables').empty();
-  initializeBoard();
-}
-
-// initialize the app
-function initialize() {
-  initializeBoard();
-  //$('#box').aToolTip();
-}
-
-// set up the jQuery data table
-function initializeTable() {
-}
+/* ****************************************************************************************************
+   JSXGraph Board functions
+*/
 
 // remove all functions from board
 function initializeBoard(boundingbox) {
@@ -124,10 +203,29 @@ function initializeBoard(boundingbox) {
   board = JXG.JSXGraph.initBoard('box', board_options);
 }
 
-// add an function as a interpolated cubic spline to the board
-function addFunction(points_data) {
+// update a function on the board according to the datatabe
+function updateFunction(elem, point_data) {
   board.suspendUpdate();
+  var data_table = elem.data().handsontable;
+  console.log('redrawing function');
+
+  // redraw the function
+  if (!data_table.listening) return;
+  $.each(data_table.drawnElements, function(i,o) {
+    if (!state.copy_mode) o.remove(); // either edit or clone the function
+  });
+  var points = redrawFunctionPoints(point_data, data_table);
+  var func = redrawFunctionGraph(points);
+  data_table.drawnElements = points.concat(func);
   
+  board.unsuspendUpdate();
+}
+
+// add a function to the datatable and board
+function addFunction(point_data) {
+  board.suspendUpdate();
+
+  // create datatable
   var elem = $('<div class="datatable"></div>')
     .appendTo('#datatables');
   elem.handsontable({
@@ -136,37 +234,34 @@ function addFunction(points_data) {
     rowHeaders: false,
     colHeaders: ['X', 'F(X)'],
     minSpareRows: 1,
-    minWidth: 400,
-    maxWidth: 20,
     fillHandle: true,
-    onChange: function(data) {
-      var data_table = elem.data().handsontable;
-      if (!data_table.listening) return;
-      console.log('changing')
-      $.each(data_table.drawnElements, function(i,o) {
-        //o.remove();
-      });
-      drawFunction(points_data, data_table);        
-    },
-    onBeforeChange: function (data) {
-      // TODO: reject non-numerical values
-      for (var i = data.length - 1; i >= 0; i--) {
-        if (data[i][3] === "nuke") return false;
-      }
-    }
+    onChange: function(data) { updateFunction(elem, point_data); },
+    onBeforeChange: function (data) { /* TODO: reject invalid values; see Handsontable demo */ }
   });
 
+  // draw the function
   var data_table = elem.data().handsontable;
-  data_table.drawnElements = drawFunction(points_data, data_table);
-  $.each(points_data, function(i,o) {
+  var points = redrawFunctionPoints(point_data, data_table);
+  var func = redrawFunctionGraph(points);
+  data_table.drawnElements = points.concat(func);
+
+  $.each(point_data, function(i,o) {
     data_table.setDataAtCell(i, 0, o[0]);
     data_table.setDataAtCell(i, 1, o[1]);
   });
+
   data_table.listening = true;
+  board.unsuspendUpdate();
 }
 
-function drawFunction(points_data, data_table) {
-  var points = $.map(points_data, function(o,i) {
+// (re)draw a function's points
+function redrawFunctionPoints(point_data, data_table) {
+
+  var x_min = point_data[0][0];
+  var x_max = point_data[point_data.length-1][0];
+
+  // draw points
+  var points = $.map(point_data, function(o,i) {
     var p = board.create('point', o, point_options);
     JXG.addEvent(p.rendNode, 'mouseup', function(e) {
       data_table.setDataAtCell(i, 0, p.coords.usrCoords[1]);
@@ -174,42 +269,46 @@ function drawFunction(points_data, data_table) {
     }, p);
     return p;
   });
-  
-  var x_min = points_data[0][0];
-  var x_max = points_data[points_data.length-1][0];
-  var f = board.create('spline', points, function_options);
-  JXG.addEvent(f.rendNode, 'click', function(e) {
-    // do stuff to the function
-  }, f);
-  
-  // We can fit a polynomial:
-  //var interpolated = board.lagrangePolynomial(p);
-  //board.create('functiongraph', [interpolated, x_min, x_max], function_options);
-  
-  // There is also code for a least-squares best-fit line:
-  // http://jsxgraph.uni-bayreuth.de/wiki/index.php/Least-squares_line_fitting
-  
-  board.unsuspendUpdate();
-  return [f].concat(points);
+
+  return points;
 }
+
+// (re)draw a function's graph
+function redrawFunctionGraph(points) {
+
+  var x_min = points[0].coords.usrCoords[1];
+  var x_max = points[points.length-1].coords.usrCoords[1];
+
+  // draw function graph
+  var func = board.create('functiongraph', [
+    state.functionmodel(points), x_min, x_max], function_options);
+  JXG.addEvent(func.rendNode, 'click', function(e) {
+    // TODO: do stuff to the function when it is clicked on, dragged, etc.
+  }, func);
+
+  return func;
+}
+
+/* ****************************************************************************************************
+   Helper functions
+*/
 
 // set a tooltip when hovering over the graph
 function setTooltip(tooltip) {
   $('#box').attr('title', tooltip);
 }
 
-$(document).ready(initialize);
-
-// helper functions - UNUSED
+// get graph coordinates from a click on the graph
 function getMouseCoords(e) {
   var cPos = board.getCoordsTopLeftCorner(e),
   absPos = JXG.getPosition(e),
   dx = absPos[0]-cPos[0],
   dy = absPos[1]-cPos[1];
-  
+
   return new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
 }
 
+// check if an element is a child of the graph element
 function isInGraph(node) {
   var n = node;
   while (n !== null && n.parentElement !== n) {
@@ -219,3 +318,6 @@ function isInGraph(node) {
   return false;
 }
 
+/* **************************************************************************************************** */
+
+$(document).ready(initialize);
